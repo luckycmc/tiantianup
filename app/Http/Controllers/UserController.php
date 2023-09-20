171da73981a93bca72c8e6e2d0129e889bc61346
dt_3926876274 <2287232750@qq.com>
2023-09-20 11:48:14 +0800

    打印日志

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -710,13 +710,8 @@ class UserController extends Controller
         $latitude = $data['latitude'] ?? 0;
         $distance_min = $data['distance_min'] ?? 0;
         $distance_max = $data['distance_max'] ?? 0;
-        Log::info('longitude: '.$longitude);
-        Log::info('latitude: '.$latitude);
-        Log::info('distance_min: '.$distance_min);
-        Log::info('distance_max: '.$distance_max);
         // 当前用户
         $user = Auth::user();
-        // $user = User::find(2);
         $where = [];
         if (isset($data['type'])) {
             $where[] = ['type','=',$data['type']];
