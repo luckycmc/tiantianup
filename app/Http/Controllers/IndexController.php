cfa8489d6ddeffbf09c7675b8677e26e4dc7a497
dt_3926876274 <2287232750@qq.com>
2023-10-06 17:08:51 +0800

    文件上传

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -29,25 +29,6 @@ use Illuminate\Support\Facades\Storage;
 class IndexController extends Controller
 {
     /**
-     * 上传文件
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public function upload()
-    {
-        $data = request()->all();
-        $file = request()->file('file');
-        $pathname = $data['pathname'] ?? 'user';
-
-        $disk = Storage::disk('cosv5');
-        $upload_path = 'upload/imgs/'.$pathname.'/' . date("Ym/d", time());
-        //将图片上传到OSS中，并返回图片路径信息 值如:imgs/1234.jpeg
-        $path = $disk->put($upload_path, $file);
-        $url = $disk->url($path);
-        $url = explode('?',$url)[0];
-        return $this->success('上传成功',compact('url'));
-    }
-
-    /**
      * 获取定位
      * @return \Illuminate\Http\JsonResponse
      */
--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -7,6 +7,7 @@ use EasyWeChat\Factory;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Redis;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
 use Overtrue\EasySms\EasySms;
 use Overtrue\EasySms\Exceptions\Exception;
@@ -145,5 +146,24 @@ class LoginController extends Controller
     {
         $data = \request()->all();
         $code = $data['code'] ?? '';
+    }
+
+    /**
+     * 上传文件
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function upload()
+    {
+        $data = request()->all();
+        $file = request()->file('file');
+        $pathname = $data['pathname'] ?? 'user';
+
+        $disk = Storage::disk('cosv5');
+        $upload_path = 'upload/imgs/'.$pathname.'/' . date("Ym/d", time());
+        //将图片上传到OSS中，并返回图片路径信息 值如:imgs/1234.jpeg
+        $path = $disk->put($upload_path, $file);
+        $url = $disk->url($path);
+        $url = explode('?',$url)[0];
+        return $this->success('上传成功',compact('url'));
     }
 }
