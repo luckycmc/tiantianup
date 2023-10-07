24e9a96b5094cb97b32a5631007f0d9dbcddee8c
dt_3926876274 <2287232750@qq.com>
2023-10-07 11:17:29 +0800

    邀请码

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -772,55 +772,21 @@ class UserController extends Controller
         if (!file_exists(public_path() . $file_name)) {
             $access_token = get_access_token();
             $request_data = [
-                'page' => 'pages/login/index?id='.$user->id,
-                // 'page' => 'pages/login/index',
+                'page' => 'pages/login/index',
                 'scene' =>  $user->id,
                 'check_path' => true,
-                // 'env_version' => 'trial'
-                'env_version' => 'release'
+                'env_version' => 'trial'
             ];
-            // dd($access_token);
 
             $request_url = 'https://api.weixin.qq.com/wxa/getwxacodeunlimit?access_token='.$access_token;
-            $request_data = json_encode($request_data,320);
-            $result = $this->_requestPost($request_url,$request_data);
-            dd($result);
+            $result = Http::post($request_url,$request_data);
+            // dd($result);
 
             $file = public_path()."/qr_code/".$user->id.".jpg";
             file_put_contents($file, $result);
+            return $this->success('邀请码',env('app_url').$file_name);
         }
-    }
-
-    public function _requestPost($url, $data, $ssl=true)
-    {
-        //curl完成
-        $curl = curl_init();
-        //设置curl选项
-        curl_setopt($curl, CURLOPT_URL, $url);//URL
-        $user_agent = isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:38.0) Gecko/20100101 Firefox/38.0 FirePHP/0.7.4';
-        curl_setopt($curl, CURLOPT_USERAGENT, $user_agent);//user_agent，请求代理信息
-        curl_setopt($curl, CURLOPT_AUTOREFERER, true);//referer头，请求来源
-        curl_setopt($curl, CURLOPT_TIMEOUT, 30);//设置超时时间
-        //SSL相关
-        if ($ssl) {
-            curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);//禁用后cURL将终止从服务端进行验证
-            curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 2);//检查服务器SSL证书中是否存在一个公用名(common name)。
-        }
-        // 处理post相关选项
-        curl_setopt($curl, CURLOPT_POST, true);// 是否为POST请求
-        curl_setopt($curl, CURLOPT_POSTFIELDS, $data);// 处理请求数据
-        // 处理响应结果
-        curl_setopt($curl, CURLOPT_HEADER, false);//是否处理响应头
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);//curl_exec()是否返回响应结果
-
-        // 发出请求
-        $response = curl_exec($curl);
-        if (false === $response) {
-            echo '<br>', curl_error($curl), '<br>';
-            return false;
-        }
-        curl_close($curl);
-        return $response;
+        return $this->success('邀请码',env('APP_URL').$file_name);
     }
 
     /**
