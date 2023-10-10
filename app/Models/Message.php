11369d2d758a5e6df5d650c8735c1a2406a74853
dt_3926876274 <2287232750@qq.com>
2023-10-10 21:41:28 +0800

    选中

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -19,7 +19,7 @@ class Message extends Model
         $model->from_user_id = $from_user_id;
         $model->name = $name;
         $model->content = $content;
-        $model->platform = $platform;
+        $model->send_platform = $platform;
         $model->status = 0;
         $where = ['user_id' => $user_id,'from_user_id' => $from_user_id,'status' => 0];
         if (!DB::table('messages')->where($where)->exists()) {
