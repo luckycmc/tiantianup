fd1fb19cc624cc5fe1a0ca1996e6f98373bae653
dt_3926876274 <2287232750@qq.com>
2023-10-10 16:38:09 +0800

    需求审核

--- app/Http/Controllers/OrganizationController.php
+++ app/Http/Controllers/OrganizationController.php
@@ -129,7 +129,7 @@ class OrganizationController extends Controller
         $data['class_date'] = json_encode($data['class_date']);
         $data['adder_role'] = 4;
         $data['adder_id'] = $user->id;
-        $data['end_time'] = $data['end_time'];
+        $data['end_time'] = $data['end_time'] ?? Carbon::now()->addDays(7);
         Log::info('data: ',$data);
         $id = DB::table('courses')->insertGetId($data);
         if (!$id) {
