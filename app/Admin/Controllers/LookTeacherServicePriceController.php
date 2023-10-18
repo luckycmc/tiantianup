b3d1910bcb12a945ea5ab43f05657a6a8360e653
dt_3926876274 <2287232750@qq.com>
2023-10-18 10:46:47 +0800

    服务费管理

--- app/Admin/Controllers/LookTeacherServicePriceController.php
+++ app/Admin/Controllers/LookTeacherServicePriceController.php
@@ -3,6 +3,7 @@
 namespace App\Admin\Controllers;
 
 use App\Admin\Repositories\ServicePrice;
+use App\Models\Region;
 use Dcat\Admin\Form;
 use Dcat\Admin\Grid;
 use Dcat\Admin\Show;
@@ -20,14 +21,12 @@ class LookTeacherServicePriceController extends AdminController
         return Grid::make(new ServicePrice(), function (Grid $grid) {
             $grid->model()->where('type',2);
             $grid->column('id')->sortable();
-            $grid->column('price');
-            $grid->column('type');
-            $grid->column('start_time');
-            $grid->column('end_time');
-            $grid->column('province');
-            $grid->column('city');
-            $grid->column('district');
-            $grid->column('adder');
+            $grid->column('price','服务费');
+            $grid->column('start_time','开始时间');
+            $grid->column('end_time','结束时间');
+            $grid->column('region','地区')->display(function () {
+                return $this->province.$this->city.$this->district;
+            });
             $grid->column('created_at');
             $grid->column('updated_at')->sortable();
         
@@ -71,14 +70,19 @@ class LookTeacherServicePriceController extends AdminController
     {
         return Form::make(new ServicePrice(), function (Form $form) {
             $form->display('id');
-            $form->text('price');
-            $form->text('type');
-            $form->text('start_time');
-            $form->text('end_time');
-            $form->text('province');
-            $form->text('city');
-            $form->text('district');
-            $form->text('adder');
+            $form->text('price','服务费');
+            $form->hidden('type')->default(2);
+            $form->dateRange('start_time','end_time','有效期');
+            $form->select('province','省')->options('/api/city')->load('city','/api/city')->saving(function ($value) {
+                return Region::where('id',$value)->value('region_name');
+            });
+            $form->select('city','市')->options('/api/city')->load('district','/api/city')->saving(function ($value) {
+                return Region::where('id',$value)->value('region_name');
+            });
+            $form->select('district','区')->options('/api/city')->saving(function ($value) {
+                return Region::where('id',$value)->value('region_name');
+            });
+            $form->text('adder','添加人');
         
             $form->display('created_at');
             $form->display('updated_at');
