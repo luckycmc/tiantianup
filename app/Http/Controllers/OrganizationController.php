183744efabcbc76b451d6492c3b6ad3386d844fd
dt_3926876274 <2287232750@qq.com>
2023-11-08 17:01:53 +0800

    机构成员

--- app/Http/Controllers/OrganizationController.php
+++ app/Http/Controllers/OrganizationController.php
@@ -59,7 +59,7 @@ class OrganizationController extends Controller
         $data['created_at'] = Carbon::now();
         $data['user_id'] = $user->id;
         $id = DB::table('organizations')->insertGetId($data);
-        $user->organ_id = id;
+        $user->organ_id = $id;
         $user->update();
         $images = \request()->input('images');
         if ($images) {
