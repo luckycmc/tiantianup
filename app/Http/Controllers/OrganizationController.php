8687cc5e557f4e4fadcb2b309919a7534c5a0036
dt_3926876274 <2287232750@qq.com>
2023-11-08 18:07:09 +0800

    机构支付

--- app/Http/Controllers/OrganizationController.php
+++ app/Http/Controllers/OrganizationController.php
@@ -757,7 +757,7 @@ class OrganizationController extends Controller
             }
             $user->withdraw_balance -= $total_amount;
             $user->update();
-            $result = '余额支付222';
+            $result = '余额支付';
         } else {
             // 调起支付
             $pay_data = [
@@ -830,7 +830,7 @@ class OrganizationController extends Controller
             $user->withdraw_balance -= $order->amount;
             $order->update();
             $user->update();
-            $result = '余额支付111';
+            $result = '余额支付';
         } else {
             // 调起支付
             $pay_data = [
