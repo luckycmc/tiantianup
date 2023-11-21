b9efc7ed184ca7cdfc6621b9e8b43e8c1640c9ec
dt_3926876274 <2287232750@qq.com>
2023-11-21 17:21:04 +0800

    推荐课程

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -172,13 +172,18 @@ class IndexController extends Controller
         $page_size = $data['page_size'] ?? 10;
         $longitude = $data['longitude'] ?? 0;
         $latitude = $data['latitude'] ?? 0;
-        // 当前城市
-        $location_info = get_location($longitude,$latitude);
-        $city = $location_info['city'];
-        $city_id = Region::where('region_name',$city)->value('id');
 
         $where = [];
-        // $where[] = ['city','=',$city_id];
+        if (isset($data['is_default'])) {
+            // 当前城市
+            $location_info = get_location($longitude,$latitude);
+            $city = $location_info['city'];
+            $city_id = Region::where('region_name',$city)->value('id');
+        } else {
+            $city_id = $data['city'];
+        }
+        $where[] = ['city','=',$city_id];
+
         if (isset($data['name'])) {
             $where[] = ['name','like','%'.$data['name'].'%'];
         }
