e6a4708951ca9e95bd167639c27654dcacc7536d
dt_3926876274 <2287232750@qq.com>
2023-11-21 17:48:22 +0800

    筛选后距离

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -60,6 +60,12 @@ class CourseController extends Controller
 
             $where[] = ['courses.city','=',$city_id];
         }
+        if (isset($data['district_id'])) {
+            $district_name = Region::where('id',$data['district_id'])->value('region_name');
+            $region_info = get_long_lat('','',$district_name,'');
+            $longitude = $region_info[0];
+            $latitude = $region_info[1];
+        }
 
         if (isset($data['fitler_type'])) {
             $where[] = ['courses.type','=',$data['fitler_type']];
