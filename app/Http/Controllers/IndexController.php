0f25b41f06b30c88e3c182fa91f677c33a493bdb
dt_3926876274 <2287232750@qq.com>
2023-12-01 16:41:42 +0800

    推荐教师

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -65,7 +65,7 @@ class IndexController extends Controller
         Log::info('district_id: '.$district_id);
         Log::info('data: ',$data);
         $page_size = $data['page_size'] ?? 10;
-        if ((isset($data['longitude']) && isset($data['latitude'])) || !isset($data['district_id'])) {
+        if ((isset($data['longitude']) && isset($data['latitude'])) && !isset($data['district_id'])) {
             // 根据经纬度获取省市区
             $location = get_location($data['longitude'],$data['latitude']);
             if (!$location) {
