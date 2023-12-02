8c1bcfd674d087bc0a1ce4baa66614929ef86483
dt_3926876274 <2287232750@qq.com>
2023-12-02 17:00:15 +0800

    家长发布需求

--- app/Http/Controllers/ParentController.php
+++ app/Http/Controllers/ParentController.php
@@ -76,7 +76,7 @@ class ParentController extends Controller
         $data['status'] = 0;
         $data['adder_role'] = 2;
         $data['adder_id'] = $user->id;
-        $carbon = Carbon::createFromFormat('Y-m-d', $data['end_time'])->setTime(23,59,59);
+        $data['end_time'] = Carbon::createFromFormat('Y-m-d', $data['end_time'])->setTime(23,59,59);
         // 价格
         $data['class_price'] = $data['class_price_min'] ?? 0;
         // 处理时间
