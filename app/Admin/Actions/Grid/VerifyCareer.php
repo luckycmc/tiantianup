03b2b22d3f070bc09fd8a279ea71d3c8782099eb
dt_3926876274 <2287232750@qq.com>
2023-12-11 19:38:44 +0800

    教师奖励

--- app/Admin/Actions/Grid/VerifyCareer.php
+++ app/Admin/Actions/Grid/VerifyCareer.php
@@ -89,7 +89,9 @@ class VerifyCareer extends RowAction
         // 查看是否有注册活动
         $teacher_activity = Activity::where(['status' => 1,'type' => 2])->where('start_time', '<=', $current)
             ->where('end_time', '>=', $current)->first();
-        if ($teacher_activity) {
+        // 查询是否已获得奖励
+        $is_reward = Activity::where(['user_id' => $user->id,'activity_id' => $teacher_activity->id,'description' => '教学经历审核通过'])->exists();
+        if ($teacher_activity && !$is_reward) {
             teacher_activity_log($teacher_info->user_id,'teacher_career_reward','教学经历','教学经历审核通过',$teacher_activity);
         }
 
