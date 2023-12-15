c3a85d548050c63a44f3899086734752cd4ec65a
dt_3926876274 <2287232750@qq.com>
2023-12-15 16:39:02 +0800

    查看需求

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -90,7 +90,7 @@ class PaymentController extends Controller
                 'out_trade_no' => $out_trade_no,
                 'description' => '服务费',
                 'amount' => [
-                    'total' => bcmul($amount,100,0),
+                    'total' => ceil($amount*100),
                     'currency' => 'CNY',
                 ],
                 'payer' => [
