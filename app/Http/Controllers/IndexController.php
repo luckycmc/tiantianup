95fac7d73426207f21e9be5d9e81126046faba0d
dt_3926876274 <2287232750@qq.com>
2023-12-25 18:19:01 +0800

    推荐教师

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -75,10 +75,11 @@ class IndexController extends Controller
             $district_id = Region::where('code',$location['adcode'])->value('id');
             $where[] = ['district_id', '=', $district_id];
         }
-        if (isset($data['city'])) {
+        if (isset($data['city']) && !isset($data['district_id'])) {
             $city_id = Region::where('region_name',$data['city'])->value('id');
             $where[] = ['city_id','=',$city_id];
         }
+        Log::info('where: ',$where);
         // 查询当前位置的所有推荐教师
         $teachers = User::with(['teacher_experience','teacher_info','teacher_education'])->where($where)->where(['is_recommend' => 1,'role' => 3,'status' => 1])->paginate($page_size);
 
