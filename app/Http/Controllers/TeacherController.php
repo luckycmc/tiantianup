ce3dcb6e4dcc87754121142a7b580031ea9ccdeb
dt_3926876274 <2287232750@qq.com>
2024-01-22 17:51:06 +0800

    需求广场

--- app/Http/Controllers/TeacherController.php
+++ app/Http/Controllers/TeacherController.php
@@ -399,12 +399,20 @@ class TeacherController extends Controller
             $result = Course::leftJoin('organizations','organizations.id','=','courses.organ_id')
                 ->leftJoin('deliver_log','deliver_log.course_id','=','courses.id')
                 ->select('courses.*','organizations.name as organ_name',DB::raw('6371 * ACOS(COS(RADIANS('.$latitude.')) * COS(RADIANS(courses.latitude)) * COS(RADIANS(courses.longitude) - RADIANS('.$longitude.')) + SIN(RADIANS('.$latitude.')) * SIN(RADIANS(courses.latitude))) AS distance'))
-                ->where($where)->where(['courses.role' => 3,'courses.status' => 1])->where('courses.adder_role','!=',0)->$condition('courses.id',$delivery_arr)->orderBy($sort_field,$order)->distinct()->paginate($page_size);
+                ->where($where)->where(['courses.role' => 3,'courses.status' => 1])->where('courses.adder_role','!=',0)->orWhere(function ($query) use ($where) {
+                    $query->where('courses.is_on',1)
+                        ->where('courses.status','!=',1)
+                        ->where('courses.method','线上');
+                })->$condition('courses.id',$delivery_arr)->orderBy($sort_field,$order)->distinct()->paginate($page_size);
         } else {
             $result = Course::leftJoin('organizations','organizations.id','=','courses.organ_id')
                 ->leftJoin('deliver_log','deliver_log.course_id','=','courses.id')
                 ->select('courses.*','organizations.name as organ_name',DB::raw('6371 * ACOS(COS(RADIANS('.$latitude.')) * COS(RADIANS(courses.latitude)) * COS(RADIANS(courses.longitude) - RADIANS('.$longitude.')) + SIN(RADIANS('.$latitude.')) * SIN(RADIANS(courses.latitude))) AS distance'))
-                ->where($where)->where(['courses.role' => 3])->where('courses.adder_role','!=',0)->orderBy($sort_field,$order)->distinct()->paginate($page_size);
+                ->where($where)->where(['courses.role' => 3])->where('courses.adder_role','!=',0)->orWhere(function ($query) use ($where) {
+                    $query->where('courses.is_on',1)
+                        ->where('courses.status','!=',1)
+                        ->where('courses.method','线上');
+                })->orderBy($sort_field,$order)->distinct()->paginate($page_size);
         }
 
         foreach ($result as $v) {
