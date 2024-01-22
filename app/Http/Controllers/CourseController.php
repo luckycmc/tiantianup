ff937a28b849073627a389a1d363b597989506da
dt_3926876274 <2287232750@qq.com>
2024-01-22 17:36:12 +0800

    课程列表

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -197,9 +197,9 @@ class CourseController extends Controller
         $result = Course::leftJoin('organizations','courses.organ_id','=','organizations.id')
             ->select($select_field)
             ->where($where)
-            ->orWhere('courses.method','线上')
-            ->where('courses.status','!=',0)
             ->where('is_on',1)
+            ->where('courses.status','!=',0)
+            ->orWhere(['courses.method'  => '线上','courses.status' => ['!=', 0],'courses.is_on' => 1])
             ->orderBy($sort_field,$order)
             ->logListenedSql()
             ->paginate($page_size);
