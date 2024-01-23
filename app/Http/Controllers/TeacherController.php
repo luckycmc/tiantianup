056a287c2ce6644ced160cb72e088b8a59b94532
dt_3926876274 <2287232750@qq.com>
2024-01-23 11:49:37 +0800

    需求列表

--- app/Http/Controllers/TeacherController.php
+++ app/Http/Controllers/TeacherController.php
@@ -401,7 +401,7 @@ class TeacherController extends Controller
                 ->select('courses.*','organizations.name as organ_name',DB::raw('6371 * ACOS(COS(RADIANS('.$latitude.')) * COS(RADIANS(courses.latitude)) * COS(RADIANS(courses.longitude) - RADIANS('.$longitude.')) + SIN(RADIANS('.$latitude.')) * SIN(RADIANS(courses.latitude))) AS distance'))
                 ->where($where)->where(['courses.role' => 3,'courses.status' => 1])->where('courses.is_on',1)->where('courses.adder_role','!=',0)->orWhere(function ($query) use ($where) {
                     $query->where('courses.is_on',1)
-                        ->where('courses.status','!=',1)
+                        ->where('courses.status','!=',0)
                         ->where('courses.method','线上');
                 })->$condition('courses.id',$delivery_arr)->orderBy($sort_field,$order)->distinct()->paginate($page_size);
         } else {
@@ -410,7 +410,7 @@ class TeacherController extends Controller
                 ->select('courses.*','organizations.name as organ_name',DB::raw('6371 * ACOS(COS(RADIANS('.$latitude.')) * COS(RADIANS(courses.latitude)) * COS(RADIANS(courses.longitude) - RADIANS('.$longitude.')) + SIN(RADIANS('.$latitude.')) * SIN(RADIANS(courses.latitude))) AS distance'))
                 ->where($where)->where(['courses.role' => 3])->where('courses.is_on',1)->where('courses.adder_role','!=',0)->orWhere(function ($query) use ($where) {
                     $query->where('courses.is_on',1)
-                        ->where('courses.status','!=',1)
+                        ->where('courses.status','!=',0)
                         ->where('courses.method','线上');
                 })->orderBy($sort_field,$order)->distinct()->paginate($page_size);
         }
